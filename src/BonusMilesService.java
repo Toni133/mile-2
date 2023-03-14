be79945b29335070df6fb8836b04380800b9a714
Anton <toniq133@bk.ru>
2023-03-14 14:34:51 +0300

    ferst comit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,10 @@
 public class BonusMilesService {
+    public int calculate(int price) {
+        int percent = 20;
+        int miles = price / percent;
+        return miles;
+        }
 
-    public calculate ()
 }
+
+
